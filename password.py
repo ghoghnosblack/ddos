313f295456582c374af60bd768c2b027f1dfb3dc
ghoghnosblack <117984289+ghoghnosblack@users.noreply.github.com>
2025-01-09 14:50:50 +0330

    password.py

--- password.py
+++ password.py
@@ -2,7 +2,7 @@ import os
 os.system('pip install requests')
 os.system('clear')
 import requests
-repo_url = "https://api.github.com/repos/09151509428/DDOS/contents/text.text"
+repo_url = 'https://github.com/ghoghnosblack/ddos/blob/main/non.text'
 response = requests.get(repo_url)
 if response.status_code == 200:
     print("        You are welcome❕")
